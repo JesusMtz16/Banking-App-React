fd4b3ffb982263153bb633e8b7fceec8026b2dc9
Jesus <jesus-mtz@hotmail.com>
2023-06-22 15:34:39 -0400

    Resolved conflicts

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { CustomerRegistration } from './Components/Customer-Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerRegistration/>
+    </div>
+  );
+}
+
+export default App;
